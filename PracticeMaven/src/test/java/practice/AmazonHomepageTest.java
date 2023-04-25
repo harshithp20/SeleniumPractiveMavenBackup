76c54aa29e12ab03b9fa8ba5e33a864b766d2d8c
TesterIndrani <131364319+TesterIndrani@users.noreply.github.com>
2023-04-25 16:55:47 +0530

    Update AmazonHomepageTest.java

--- PracticeMaven/src/test/java/practice/AmazonHomepageTest.java
+++ PracticeMaven/src/test/java/practice/AmazonHomepageTest.java
@@ -2,14 +2,15 @@ package practice;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AmazonHomepageTest {
 
 	@Test
 	public void amazonHomepageTest()
 	{
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+	//	WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/"); 
 	}
 }
